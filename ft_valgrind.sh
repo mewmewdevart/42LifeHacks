40443f5c50c012e7d86f8ecaf47d5c15da603bd8
Meowbutu <mewmewdevart@gmail.com>
2023-03-19 13:30:01 -0300

    using chmod 777

--- ft_valgrind.sh
+++ ft_valgrind.sh
@@ -9,20 +9,15 @@ echo "   \ \__/ /     \ \__\ \__\ \_______\ \_______\ \__\\ _\\ \__\ \__\\ \__\ 
 echo "    \|__|/       \|__|\|__|\|_______|\|_______|\|__|\|__|\|__|\|__| \|__|\|_______| "
 
 
-# Entrando na pasta bin
+# (baseado nos meus files)
 cd bin
 
-# Obtendo o primeiro arquivo encontrado
 first_file=$(find . -maxdepth 1 -type f | head -n 1)
 
-# Executando o valgrind com o arquivo selecionado e salvando os resultados
 valgrind --track-origins=yes ./"$first_file" > result.txt 2> leaks.txt
 
-# Filtrando os erros encontrados
 grep "ERROR SUMMARY" result.txt > errors.txt
 
-# Comparando com um arquivo de teste
 diff teste.txt errors.txt > result.diff
 
-# Exibindo o resultado da comparação
 cat result.diff
